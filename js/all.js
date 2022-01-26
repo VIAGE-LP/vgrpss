f66fc23f7902c1b55e07125c0f8ea9161d1d3c85
Pickachu Chen <pickachu1006@gmail.com>
2022-01-26 10:21:29 +0800

    20220126_close popup

--- js/all.js
+++ js/all.js
@@ -8,15 +8,15 @@ $(document).ready(function () {
     });    
 
     //週年慶活動辦法下拉選單
-    $(".anniversary_2,.SP_anniversary_2").hide();
-    $('.anniversary_1, .SP_anniversary_1').click(function (e) {
-        e.preventDefault();
-        $(".anniversary_2,.SP_anniversary_2").toggle();;
-    });
-    $('.anniversary_2, .SP_anniversary_2').click(function (e) {
-        e.preventDefault();
-        $(".anniversary_2, .SP_anniversary_2").hide();
-    });
+    // $(".anniversary_2,.SP_anniversary_2").hide();
+    // $('.anniversary_1, .SP_anniversary_1').click(function (e) {
+    //     e.preventDefault();
+    //     $(".anniversary_2,.SP_anniversary_2").toggle();;
+    // });
+    // $('.anniversary_2, .SP_anniversary_2').click(function (e) {
+    //     e.preventDefault();
+    //     $(".anniversary_2, .SP_anniversary_2").hide();
+    // });
 
     // $("#wh-call-to-action").removeAttr("wh-click");
     $("#wh-call-to-action").removeClass("wh-animation-in");
